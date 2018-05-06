1e20dd70153130c494e43c08831858196eebf9f9
Heather Danielson <sayhello.heather@gmail.com>
2018-05-06 13:04:25 -0400

    Added Basic auth info
    Signed-off-by: Heather Danielson <sayhello.heather@gmail.com>

--- HttpClientServiceUtil.java
+++ HttpClientServiceUtil.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
 
 import org.json.simple.JSONObject; //json-simple-1.1.1.jar
 
@@ -16,6 +17,8 @@ import org.json.simple.JSONObject; //json-simple-1.1.1.jar
 public class HttpClientServiceUtil {
  
 	private final String USER_AGENT = "NotThatSpecial";
+	String encoding = Base64.getEncoder().encodeToString(("<user>:<pwd>").getBytes());
+
  
 	public static void main(String[] args) throws Exception {
 
@@ -40,6 +43,7 @@ public class HttpClientServiceUtil {
 
 		//add request header
 		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty  ("Authorization", "Basic " + encoding);
 
 		int responseCode = con.getResponseCode();
 		System.out.println("Sending get request : "+ url);
@@ -72,6 +76,7 @@ public class HttpClientServiceUtil {
 		con.setRequestProperty("User-Agent", USER_AGENT);
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
 		con.setRequestProperty("Content-Type","application/json");
+		con.setRequestProperty  ("Authorization", "Basic " + encoding);
 
 		String postJsonData;
 		/*String representation of JSON data ex:
